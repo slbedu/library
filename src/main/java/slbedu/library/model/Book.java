49777570a0b6e6a947fdaa7679b7c9cf79cb9739
Ivan St. Ivanov <ivan_st_ivanov@yahoo.com>
2015-01-22 20:24:28 +0200

    Created the project entities

---
+++ src/main/java/slbedu/library/model/Book.java
@@ -0,0 +1,119 @@
+package slbedu.library.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Entity
+public class Book implements Serializable
+{
+
+   @Id
+   @GeneratedValue(strategy = GenerationType.AUTO)
+   private Long id;
+
+   private String title;
+
+   private String author;
+
+   private String isbn;
+
+   private int amount;
+
+   public Long getId()
+   {
+      return this.id;
+   }
+
+   public void setId(final Long id)
+   {
+      this.id = id;
+   }
+
+   public String getTitle()
+   {
+      return title;
+   }
+
+   public void setTitle(String title)
+   {
+      this.title = title;
+   }
+
+   public String getAuthor()
+   {
+      return author;
+   }
+
+   public void setAuthor(String author)
+   {
+      this.author = author;
+   }
+
+   public String getIsbn()
+   {
+      return isbn;
+   }
+
+   public void setIsbn(String isbn)
+   {
+      this.isbn = isbn;
+   }
+
+   public int getAmount()
+   {
+      return amount;
+   }
+
+   public void setAmount(int amount)
+   {
+      this.amount = amount;
+   }
+
+   @Override
+   public String toString()
+   {
+      String result = getClass().getSimpleName() + " ";
+      if (title != null && !title.trim().isEmpty())
+         result += "title: " + title;
+      if (author != null && !author.trim().isEmpty())
+         result += ", author: " + author;
+      if (isbn != null && !isbn.trim().isEmpty())
+         result += ", isbn: " + isbn;
+      result += ", amount: " + amount;
+      return result;
+   }
+
+   @Override
+   public boolean equals(Object obj)
+   {
+      if (this == obj)
+      {
+         return true;
+      }
+      if (!(obj instanceof Book))
+      {
+         return false;
+      }
+      Book other = (Book) obj;
+      if (id != null)
+      {
+         if (!id.equals(other.id))
+         {
+            return false;
+         }
+      }
+      return true;
+   }
+
+   @Override
+   public int hashCode()
+   {
+      final int prime = 31;
+      int result = 1;
+      result = prime * result + ((id == null) ? 0 : id.hashCode());
+      return result;
+   }
+}
---
+++ src/main/java/slbedu/library/model/User.java
@@ -0,0 +1,133 @@
+package slbedu.library.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "USERS")
+public class User implements Serializable
+{
+
+   @Id
+   @GeneratedValue(strategy = GenerationType.AUTO)
+   private Long id;
+
+   private String userName;
+
+   private String password;
+
+   private String email;
+
+   @Temporal(TemporalType.DATE)
+   private Date dateOfBirth;
+
+   @OneToMany
+   private Set<Book> currentBooks = new HashSet<Book>();
+
+   public Long getId()
+   {
+      return this.id;
+   }
+
+   public void setId(final Long id)
+   {
+      this.id = id;
+   }
+
+   public String getUserName()
+   {
+      return userName;
+   }
+
+   public void setUserName(String userName)
+   {
+      this.userName = userName;
+   }
+
+   public String getPassword()
+   {
+      return password;
+   }
+
+   public void setPassword(String password)
+   {
+      this.password = password;
+   }
+
+   public String getEmail()
+   {
+      return email;
+   }
+
+   public void setEmail(String email)
+   {
+      this.email = email;
+   }
+
+   public Date getDateOfBirth()
+   {
+      return dateOfBirth;
+   }
+
+   public void setDateOfBirth(Date dateOfBirth)
+   {
+      this.dateOfBirth = dateOfBirth;
+   }
+
+   @Override
+   public String toString()
+   {
+      String result = getClass().getSimpleName() + " ";
+      if (userName != null && !userName.trim().isEmpty())
+         result += "userName: " + userName;
+      if (password != null && !password.trim().isEmpty())
+         result += ", password: " + password;
+      if (email != null && !email.trim().isEmpty())
+         result += ", email: " + email;
+      return result;
+   }
+
+   public Set<Book> getCurrentBooks()
+   {
+      return this.currentBooks;
+   }
+
+   public void setCurrentBooks(final Set<Book> currentBooks)
+   {
+      this.currentBooks = currentBooks;
+   }
+
+   @Override
+   public boolean equals(Object obj)
+   {
+      if (this == obj)
+      {
+         return true;
+      }
+      if (!(obj instanceof User))
+      {
+         return false;
+      }
+      User other = (User) obj;
+      if (id != null)
+      {
+         if (!id.equals(other.id))
+         {
+            return false;
+         }
+      }
+      return true;
+   }
+
+   @Override
+   public int hashCode()
+   {
+      final int prime = 31;
+      int result = 1;
+      result = prime * result + ((id == null) ? 0 : id.hashCode());
+      return result;
+   }
+}
