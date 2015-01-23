5d0598f6365292c943fbc0af638e966cbac822b3
Kiril Gavrailov <k.gavrailov@sap.com>
2015-01-23 11:01:02 +0200

    Added main method functionalities, database utils and enchanced the donate and borrow book a little

--- src/main/java/slbedu/library/model/User.java
+++ src/main/java/slbedu/library/model/User.java
@@ -1,141 +1,125 @@
 package slbedu.library.model;
 
+import javax.persistence.*;
+
 import java.io.Serializable;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
-
 @Entity
 @Table(name = "USERS")
-public class User implements Serializable
-{
+public class User implements Serializable {
+	private static final long serialVersionUID = 1L;
 
-   @Id
-   @GeneratedValue(strategy = GenerationType.AUTO)
-   private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
 
-   private String userName;
+	private String userName;
 
-   private String password;
+	private String password;
 
-   private String email;
+	private String email;
 
-   @Temporal(TemporalType.DATE)
-   private Date dateOfBirth;
+	@Temporal(TemporalType.DATE)
+	private Date dateOfBirth;
 
-   @OneToMany
-   private Set<Book> currentBooks = new HashSet<Book>();
+	@OneToMany
+	private Set<Book> currentBooks = new HashSet<Book>();
 
-   public Long getId()
-   {
-      return this.id;
-   }
+	public User() {
+	}
 
-   public void setId(final Long id)
-   {
-      this.id = id;
-   }
+	public User(String userName, String password, String email, Date dateOfBirth) {
+		this.userName = userName;
+		this.password = password;
+		this.email = email;
+		this.dateOfBirth = dateOfBirth;
+	}
 
-   public String getUserName()
-   {
-      return userName;
-   }
+	public Long getId() {
+		return this.id;
+	}
 
-   public void setUserName(String userName)
-   {
-      this.userName = userName;
-   }
+	public void setId(final Long id) {
+		this.id = id;
+	}
 
-   public String getPassword()
-   {
-      return password;
-   }
+	public String getUserName() {
+		return userName;
+	}
 
-   public void setPassword(String password)
-   {
-      this.password = password;
-   }
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
 
-   public String getEmail()
-   {
-      return email;
-   }
+	public String getPassword() {
+		return password;
+	}
 
-   public void setEmail(String email)
-   {
-      this.email = email;
-   }
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
-   public Date getDateOfBirth()
-   {
-      return dateOfBirth;
-   }
+	public String getEmail() {
+		return email;
+	}
 
-   public void setDateOfBirth(Date dateOfBirth)
-   {
-      this.dateOfBirth = dateOfBirth;
-   }
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
-   @Override
-   public String toString()
-   {
-      String result = getClass().getSimpleName() + " ";
-      if (userName != null && !userName.trim().isEmpty())
-         result += "userName: " + userName;
-      if (password != null && !password.trim().isEmpty())
-         result += ", password: " + password;
-      if (email != null && !email.trim().isEmpty())
-         result += ", email: " + email;
-      return result;
-   }
+	public Date getDateOfBirth() {
+		return dateOfBirth;
+	}
 
-   public Set<Book> getCurrentBooks()
-   {
-      return this.currentBooks;
-   }
+	public void setDateOfBirth(Date dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
 
-   public void setCurrentBooks(final Set<Book> currentBooks)
-   {
-      this.currentBooks = currentBooks;
-   }
+	@Override
+	public String toString() {
+		String result = getClass().getSimpleName() + " ";
+		if (userName != null && !userName.trim().isEmpty())
+			result += "userName: " + userName;
+		if (password != null && !password.trim().isEmpty())
+			result += ", password: " + password;
+		if (email != null && !email.trim().isEmpty())
+			result += ", email: " + email;
+		return result;
+	}
 
-   @Override
-   public boolean equals(Object obj)
-   {
-      if (this == obj)
-      {
-         return true;
-      }
-      if (!(obj instanceof User))
-      {
-         return false;
-      }
-      User other = (User) obj;
-      if (id != null)
-      {
-         if (!id.equals(other.id))
-         {
-            return false;
-         }
-      }
-      return true;
-   }
+	public Set<Book> getCurrentBooks() {
+		return this.currentBooks;
+	}
 
-   @Override
-   public int hashCode()
-   {
-      final int prime = 31;
-      int result = 1;
-      result = prime * result + ((id == null) ? 0 : id.hashCode());
-      return result;
-   }
+	public void setCurrentBooks(final Set<Book> currentBooks) {
+		this.currentBooks = currentBooks;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (!(obj instanceof User)) {
+			return false;
+		}
+		User other = (User) obj;
+		if (id != null) {
+			if (!id.equals(other.id)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
 }
