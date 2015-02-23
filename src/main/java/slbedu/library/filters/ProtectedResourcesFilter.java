95ee1ca95b2c04f8ca3a4c8d2807e49c2ee3fceb
Kiril Gavrailov <k.gavrailov@sap.com>
2015-02-23 19:30:31 +0200

    Add web filter to check if an user is present when borrowing book

--- src/main/java/slbedu/library/filters/ProtectedResourcesFilter.java
+++ src/main/java/slbedu/library/filters/ProtectedResourcesFilter.java
@@ -24,7 +24,6 @@ public class ProtectedResourcesFilter implements Filter {
 
     @Inject
     UserContext userContext;
-<<<<<<< HEAD
 
     public void init(FilterConfig fConfig) throws ServletException {
     }
@@ -53,33 +52,4 @@ public class ProtectedResourcesFilter implements Filter {
 
     public void destroy() {
     }
-=======
-    
-    public void init(FilterConfig fConfig) throws ServletException {
-    }
-
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        if(!isHttpCall(request, response)) { 
-            return;
-        }
-	    HttpServletResponse httpServletResponse = (HttpServletResponse) response;
-	    HttpServletRequest httpServletRequest = (HttpServletRequest) request;
-	    User currentUser = userContext.getCurrentUser();
-	    if(currentUser == null) { 
-	        String loginUrl = httpServletRequest.getContextPath() + "/login.html";
-            httpServletResponse.sendRedirect(loginUrl);
-	        return;
-	    }
-		chain.doFilter(request, response);
-	}
-
-    private boolean isHttpCall(ServletRequest request, ServletResponse response) {
-        return (request instanceof HttpServletRequest) && (response instanceof HttpServletResponse);
-    }
-
-	public void destroy() {
-	}
-	
-
->>>>>>> Add web filter to check if an user is present when borrowing book
 }
